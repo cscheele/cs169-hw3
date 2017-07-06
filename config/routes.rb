608c131db83cdf3849a95dc502f2a6a6af6ebc8b
Claire Scheele <cscheele@berkeley.edu>
2017-07-06 19:32:52 +0000

    Changed syntax in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'movies#index'
   
-  get '/sort/:id', to: 'movies#sort', as: 'type'
+  get '/sort/:id' => 'movies#sort', as: :type
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
