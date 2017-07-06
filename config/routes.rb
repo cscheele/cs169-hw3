712be75938464a0835bfafcd751972a7cd66f814
Claire Scheele <cscheele@berkeley.edu>
2017-07-06 19:57:26 +0000

    moved sort route get block under resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'movies#index'
   
-  #get '/sort/:id' => 'movies#sort', as: :type
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -16,7 +15,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :movies
+  resources :movies do
+    get '/sort/:id' => 'movies#sort'
+  end
   
   # Example resource route with options:
   #   resources :products do
